9fac35e69435bd2e0d1875a6484f71e57af42e03
1stMan <1stmanleader@gmail.com>
2022-07-05 08:40:05 +0900

    CleaningInventory Code

--- Assets/EasyNpcs/Inventory-master/Unity Project/Packages/FarrokGames.Inventory/Runtime/Pool.cs
+++ Assets/EasyNpcs/Inventory-master/Unity Project/Packages/FarrokGames.Inventory/Runtime/Pool.cs
@@ -4,75 +4,46 @@ using System.Linq;
 
 namespace FarrokhGames.Shared
 {
-    /// <summary>
-    /// A generic pool of objects that can be retrieved and recycled without invoking additional allocations.
-    /// 
-	/// Please note that care must be taken when pooling objects, since the object
-	/// has to be manually reset after retrieval from the pool. Its constructor will
-	/// not be run again after the first time!
-    /// </summary>
     public sealed class Pool<T> where T : class
     {
-        private List<T> _inactive = new List<T>();
-        private List<T> _active = new List<T>();
-        private Func<T> _creator;
+        private List<T> inactive_imageObjects = new List<T>();
+        private List<T> active_imageObjects = new List<T>();
+        private Func<T> default_image_object;
         private bool _allowTakingWhenEmpty;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="creator">A function to be used for creating new objects</param>
-        /// <param name="initialCount">Initial count of objects in the pool</param>
-        /// <param name="allowTakingWhenEmpty">If true, objects can be taken from the pool even when the pool is empty</param>
-        public Pool(Func<T> creator, int initialCount = 0, bool allowTakingWhenEmpty = true)
+        public Pool(Func<T> imageObject, int initialCount = 0, bool allowTakingWhenEmpty = true)
         {
-            if (creator == null) throw new ArgumentNullException("pCreator");
+            if (imageObject == null) throw new ArgumentNullException("pCreator");
             if (initialCount < 0) throw new ArgumentOutOfRangeException("pInitialCount", "Initial count cannot be negative");
 
-            _creator = creator;
-            _inactive.Capacity = initialCount;
+            default_image_object = imageObject;
+            inactive_imageObjects.Capacity = initialCount;
             _allowTakingWhenEmpty = allowTakingWhenEmpty;
 
-            // Create initial items
-            while (_inactive.Count < initialCount)
+            Create_DefaultImageObject_Much_As_InitialCount(initialCount);
+        }
+
+        void Create_DefaultImageObject_Much_As_InitialCount(int initialCount)
+        {
+            while (inactive_imageObjects.Count < initialCount)
             {
-                _inactive.Add(_creator());
+                inactive_imageObjects.Add(default_image_object());
             }
         }
 
-        /// <summary>
-        /// Returns the current number of objects in the pool
-        /// </summary>
-        public int Count => _inactive.Count;
-
-        /// <summary>
-        /// Returns true if the pool is empty.
-		/// Note that objects can still be taken from an empty pool, if allowTakingWhenEmpty 
-        /// is set to true, but that additional allocations will be imposed.
+        public int Count => inactive_imageObjects.Count;
         public bool IsEmpty => Count == 0;
-
-        /// <summary>
-        /// Returns true of an item can be taken from the pool right now.
-        /// This is not the same as whether or not the pool is empty.
-        /// </summary>
         public bool CanTake => _allowTakingWhenEmpty || !IsEmpty;
 
-        /// <summary>
-        /// Takes an object from the pool.
-        /// If the pool is empty and allowTakingWhenEmpty is enabled, it will allocate
-        /// a new object. Otherwise, a pooled object is returned. Either way, the object 
-        /// should eventually be returned to the pool by calling Recycle().
-        /// </summary>
-        /// <returns>An object of type T, or null if nothing could be taken</returns>
         public T Take()
         {
             if (IsEmpty)
             {
                 if (_allowTakingWhenEmpty)
                 {
-                    var obj = _creator();
-                    _inactive.Add(obj);
-                    return TakeInternal();
+                    var obj = default_image_object();
+                    inactive_imageObjects.Add(obj);
+                    return Activated_ImageObject();
                 }
                 else
                 {
@@ -81,15 +52,16 @@ namespace FarrokhGames.Shared
             }
             else
             {
-                return TakeInternal();
+                return Activated_ImageObject();
             }
         }
 
-        private T TakeInternal()
+        T Activated_ImageObject()
         {
-            T obj = _inactive[_inactive.Count - 1];
-            _inactive.RemoveAt(_inactive.Count - 1);
-            _active.Add(obj);
+            T obj = inactive_imageObjects[inactive_imageObjects.Count - 1];
+            inactive_imageObjects.RemoveAt(inactive_imageObjects.Count - 1);
+            active_imageObjects.Add(obj);
+
             return obj;
         }
 
@@ -100,9 +72,9 @@ namespace FarrokhGames.Shared
         /// </summary>
         public void Recycle(T item)
         {
-            if (!_active.Contains(item)) { throw new InvalidOperationException("An item was recycled even though it was not part of the pool"); }
-            _inactive.Add(item);
-            _active.Remove(item);
+            if (!active_imageObjects.Contains(item)) { throw new InvalidOperationException("An item was recycled even though it was not part of the pool"); }
+            inactive_imageObjects.Add(item);
+            active_imageObjects.Remove(item);
         }
 
         /// <summary>
@@ -110,7 +82,7 @@ namespace FarrokhGames.Shared
         /// </summary>
         public List<T> GetInactive()
         {
-            return _inactive.ToList();
+            return inactive_imageObjects.ToList();
         }
 
         /// <summary>
@@ -118,7 +90,7 @@ namespace FarrokhGames.Shared
         /// </summary>
         public List<T> GetActive()
         {
-            return _active.ToList();
+            return active_imageObjects.ToList();
         }
     }
 }
